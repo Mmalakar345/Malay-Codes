7ea1b3e6482953087c99d189dba5471fca94b55a
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2022-06-11 17:55:50 +0530

    Add files via upload

---
+++ Python/C--loop/4_for_loop.py
@@ -0,0 +1,13 @@
+
+for num in[1,3,5,7,9,11]:
+     print("num is=",num)
+
+
+
+
+for num in[1,3,5,7,9,11]:
+    if num>1:
+        print()
+        print("num is=",num)
+    else:
+        print("num is=",num)
