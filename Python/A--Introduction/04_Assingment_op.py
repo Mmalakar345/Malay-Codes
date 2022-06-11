47b2602ee6b222eb521a0319dfb03e0b39734644
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2022-06-11 17:27:13 +0530

    Add files via upload

---
+++ Python/A--Introduction/04_Assingment_op.py
@@ -0,0 +1,49 @@
+
+                                 #assignment operator
+
+
+Third=7
+Forth=2
+
+
+Forth+=Third               
+print(Forth)        #forth = forth + third         
+
+Forth-=Third
+print(Forth)        #a=a-b
+                           
+Forth*=Third              
+print(Forth)        #a=a*b        
+
+Forth/=Third
+print(Forth)        #a=a/b
+                         
+Forth//=Third              
+print(Forth)        #a=a//b        
+
+Forth%=Third
+print(Forth)        #a=a%b 
+
+Forth**=Third
+print(Forth)     #a=a**b
+
+
+
+
+'''Forth&=Third
+print(Forth)
+
+Forth|=Third
+print(Forth)
+
+Forth^=Third    
+print(Forth)
+                
+Forth>>=Third
+print(Forth)    
+
+Forth<<=Third
+print(Forth)'''
+
+
+
