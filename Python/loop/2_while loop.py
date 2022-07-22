ed6bf23ef536ef9d4a7e3819565a51a7f2f9a5d3
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2022-07-22 23:08:19 +0530

    Add files via upload

---
+++ Python/loop/2_while loop.py
@@ -0,0 +1,36 @@
+# Display loop wise counting "string" in new line
+
+count=1
+while count <=5 :         # while operation exsicute korbar jonno true condition
+     print("Hello")       # sting a jai likha hok tai print hobe loop jotokhon cholbe
+     count=count+1       # i=i+1  same as i+=1
+
+
+
+count=1
+while count <=5 :         # while operation exsicute korbar jonno true condition
+     print("Programmer",end=' ')       # sting a jai likha hok tai print hobe loop jotokhon cholbe
+     count=count+1       # i=i+1  same as i+=1
+
+
+# Write a programe to generate the following seris:
+# 1,4,7,10,13......... up 10 terms
+
+count=1
+term=1
+while count<=5 :
+     print(term)
+     term=term+3
+     count=count+1
+
+# Write a programe to generate the following seris:
+# 1,4,7,10,13......... up 10 terms
+# output is horizontal not vertical
+
+
+count=1
+term=1
+while count<=15 :
+     print(term,end=" - ")
+     term=term+3
+     count=count+1 
