424985b10d67399174241ff41c1006982117cd16
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2022-07-22 23:09:09 +0530

    Add files via upload

---
+++ Python/Number/14_Continue_loop.py
@@ -0,0 +1,36 @@
+# 1. Print 1 to 10 using continue
+
+# for count in range(1,11):
+#     if count%5==0:
+#         continue
+#     else:
+#         print(count)
+
+# 2. Print 1 to 10 using continue + other thing add
+
+
+# for count in range(1,11):
+#     if count%5==0:
+#         continue
+#         print("hi")
+#     else:
+#         print(count)
+#         print("bye")
+
+# 3. Print using for loop 1 to 10 using continue and without using if else
+
+# for i in range(1,11):
+#     print("hi")
+#     continue
+#     print("bye")
+
+
+# 4. Print using while loop 1 to 10 using continue and without using if else
+count=1
+while count<=10:
+    print("hi")
+    continue
+    print("bye")  # infinite loop hoye jbe karon
+    count+=1      # continue er por loop ta count+=1 part a asbei na
+    
+    
---
+++ Python/Number/3_special_num_ck.py
@@ -0,0 +1,38 @@
+#Input anumber more than 9 and check whether it is a special number or not 
+# #A Special number is a number whose sum of odd digits equals to Sum of even digits 
+# #Input: 2343
+# #output: It is a Special number 
+# #Input:1234 
+# Output: It is not a Special number 
+# Input: 5 
+# output: Invalid Input 
+# 2 3 4 3
+# odd   even
+# 3+3   4+2
+# 6       6
+# odd  = even
+# special num
+num=int(input("Enter a number more than 9= ")) 
+evensum=0
+oddsum=0
+if num<10:
+    print("Invalide Input")
+else:
+    while num>0:       # 10 diye mode korle last digit tai mode hoy 2343= mode 3
+    # for di in range(num,0,):
+        digit=num%10  # digit extraction
+        if digit%2==0:  # even number k 2 diye mode korle remainder 0 hoy
+            evensum+=digit
+        else:
+            oddsum+=digit
+        print(num)
+        num//=10   # floor divisioon kore last digit bad jai 221//= 22
+    if evensum==oddsum:
+        print("output: It is a special number")
+    else:
+        print("output: It is not a special number")
+        
+        
+
+
+
