48ca04fa51c227871d4eeb52eab2c74866bc4acb
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2022-09-12 11:10:12 +0530

    Add files via upload

---
+++ Python/Introduction/04_Assingment_op.py
@@ -0,0 +1,52 @@
+                                 #assignment operator
+Third=7
+Forth=2
+"***********************************************"
+Forth+=Third               
+print("Forth+=Third= ",Forth)        #forth = forth + third         
+"***********************************************"
+Forth-=Third
+print("Forth-=Third= ",Forth)        #a=a-b
+"***********************************************"                          
+Forth*=Third              
+print("Forth*=Third= ",Forth)        #a=a*b        
+"***********************************************"
+Forth/=Third
+print("Forth/=Third= ",Forth)        #a=a/b
+"***********************************************"                        
+Forth//=Third              
+print("Forth//=Third= ",Forth)        #a=a//b        
+"***********************************************"
+Forth%=Third
+print("Forth%=Third= ",Forth)        #a=a%b 
+"***********************************************"
+Forth**=Third
+print("Forth**=Third= ",Forth)     #a=a**b
+
+# Assingment operator use kore amra kokonoi left side a int right a variable 
+# possible na (add=2) (sum=5)
+# (2+=add)2+=add
+# left a variable diye right a ja kisu likha jai
+# (add=+2) (add+=sum) 
+
+# Declearing many variable in the same line
+n1,n2,n3=1,2,3
+print("n1+n2+n3= ",n1+n2+n3)
+
+'''Forth&=Third
+print(Forth)
+
+Forth|=Third
+print(Forth)
+
+Forth^=Third    
+print(Forth)
+                
+Forth>>=Third
+print(Forth)    
+
+Forth<<=Third
+print(Forth)'''
+
+
+
