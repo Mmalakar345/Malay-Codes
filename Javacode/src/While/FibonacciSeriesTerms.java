f87e7e490be4c4681484e42ca01a9a34bef7fa41
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2023-07-22 21:29:18 +0530

    Update FibonacciSeriesTerms.java

--- Javacode/src/While/FibonacciSeriesTerms.java
+++ Javacode/src/While/FibonacciSeriesTerms.java
@@ -8,7 +8,7 @@ public class FibonacciSeriesTerms {
 	// Write a java program to print fibonacci series with max value.
 		Scanner sc = new Scanner(System.in);
 		int n,x=0,y=1,z=0,term=1;
-		System.out.println("Enter how many term you wanted to print");
+		System.out.println("Enter how many term you wanted to print ");
 		n = sc.nextInt();
 		while (term<=n)
 		{
