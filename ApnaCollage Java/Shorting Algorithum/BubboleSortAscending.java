f08c9020c36071e0728d43d82e01340b89a805db
Malay Malakar <105651337+Mmalakar345@users.noreply.github.com>
2024-12-14 20:58:23 +0530

    Add files via upload

---
+++ ApnaCollage Java/Shorting Algorithum/BubboleSortAscending.java
@@ -0,0 +1,24 @@
+package SortingAlgorithum;
+
+import java.util.Arrays;
+
+public class BubboleSortAscending {
+    public static void bubboleSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 8, 7, 1, -2 };
+        int n = arr.length;
+        bubboleSort(arr, n);
+    }
+}
